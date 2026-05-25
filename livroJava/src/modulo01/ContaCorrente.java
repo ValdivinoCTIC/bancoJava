ade6f2907416d48decfe38233e5d86ebdb156b7f
ValdivinoCTIC <valdivinoctic@gmail.com>
2026-05-25 19:17:19 -0300

    Classes em Java_

--- livroJava/src/modulo01/ContaCorrente.java
+++ livroJava/src/modulo01/ContaCorrente.java
@@ -1,5 +1,52 @@
 package modulo01;
 
 public class ContaCorrente {
-
+	int conta;
+	int agencia;
+	double saldo;
+	String nome;
+	
+	public String getNome(){
+		return this.nome;
+	}
+	
+	public void setNome(String nome){
+		this.nome = nome;
+	}
+	
+	void efetuarSaque(double valor){
+		this.saldo = this.saldo - valor;
+	}
+	
+	void efetuarDeposito(double valor){
+		this.saldo = this.saldo + valor;
+	}
+	
+	void imprimirSaldo(){
+		System.out.println(this.saldo);
+	}
+	
+	public int getAgencia(){
+		return this.agencia;
+	}
+	
+	public void setAgencia(int agencia){
+		this.agencia = agencia;
+	}
+	
+	public int getConta(){
+		return this.conta;
+	}
+	
+	public void setConta(int conta){
+		this.conta = conta;
+	}
+	
+	public double getSaldo(){
+		return this.saldo;
+	}
+	
+	public void setSaldo(double saldo){
+		this.saldo = saldo;
+	}
 }
