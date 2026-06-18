fb27a4330b438435b9f60662bd8668420e769c42
ValdivinoCTIC <valdivinoctic@gmail.com>
2026-06-18 07:16:50 -0300

    ExemploIf

---
+++ livroJava/src/modulo01/ExemploIf.java
@@ -0,0 +1,11 @@
+package modulo01;
+
+public class ExemploIf {
+	public static void main(String[] args) {
+		int var1 = 20;
+		int var2 = 10;
+		if(var1 > var2) {
+			System.out.println("var1 é maior que var2");
+		}
+	}
+}
